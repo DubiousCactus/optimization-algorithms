9e7c6479a0075fbc2390ae36fdb303535234bbf1
Theo Morales <theo.morales.fr@gmail.com>
2017-12-01 10:14:30 +0100

    Import vector correctly (from petr's results, he's the man)

--- Main.cpp
+++ Main.cpp
@@ -12,14 +12,14 @@ int main(int argc, char  **argv) {
 	std::cout << "--- Using ORL dataset ---" << std::endl << std::endl;
 
 	ORLData *faces = new ORLData(40, 30, 40);
-	faces->loadDirectory("/home/transpalette/Code/Optimization_Project/ORL");
+	faces->loadDirectory("/home/transpalette/Code/optimization-algorithms/ORL");
 
 	int ch = std::cin.get();
 
 	std::cout << "--- Using MNIST dataset ---" << std::endl << std::endl;
 	
 	MNISTData *digits = new MNISTData(10, 28, 28);
-	digits->loadDirectory("/home/transpalette/Code/Optimization_Project/MNIST"); //Use full path
+	digits->loadDirectory("/home/transpalette/Code/optimization-algorithms/MNIST"); //Use full path
 
 	std::cout << std::endl << "* Running Nearest Class Centroid..." << std::endl;
 	
--- ORLData.cpp
+++ ORLData.cpp
@@ -20,9 +20,12 @@ void ORLData::visualiseImageVector(Eigen::VectorXd image_vector) {
     /* Build the matrix from the vector */
     double image_matrix[mWidth][mHeight];
 
-    for (int i = 0; i < image_vector.size() / mHeight; i++)
-        for (int j = 0; j < mHeight; j++)
-            image_matrix[i][j] = image_vector(j + ((mHeight - 1) * i));
+    for (int i = 0; i < mWidth; i++) {
+        for (int j = 0; j < mHeight; j++) {
+            image_matrix[i][j] = image_vector(j + (mHeight * i));
+	    std::cout  << "Matrix[" << i << "][" << j << "] = image_vector[" << j + (mHeight * i) << "]" << std::endl;
+	}
+    }
 
     CImg<double> image((const double*) image_matrix, mWidth, mHeight, 1, 1, true);
     image.display("Image");
@@ -32,7 +35,7 @@ void ORLData::visualiseImageVector(Eigen::VectorXd image_vector) {
 void ORLData::loadDirectory(std::string path) {
 	std::ifstream ocl_data_file, ocl_labels_file;
 	ocl_data_file.open(path + "/orl_data.txt", std::ios::in);
-	ocl_labels_file.open(path + "/orl_lbls.txt", std::ios::in);
+	ocl_labels_file.open(path + "/train_labels_ORL.dat", std::ios::in);
 
 	if (ocl_data_file.is_open() && ocl_labels_file.is_open()) {
 		/* Looks like the file contains values of 8 characters, separated by tabs */
@@ -44,11 +47,11 @@ void ORLData::loadDirectory(std::string path) {
 		size_t pos;
 		std::string delimiter = "\t";
 
-		/*while (!ocl_data_file.eof()) {
-			 *//* Read line by line. One line = 400 vectors/pictures *//*
+		while (!ocl_data_file.eof()) {
+			//* Read line by line. One line = 400 vectors/pictures /*
 			while (std::getline(ocl_data_file, line)) {
-				 *//* Split line in values, separated by tabs,
- 				  * and add those values to the corresponding vectors *//*
+				 /*Split line in values, separated by tabs,
+				  and add those values to the corresponding vectors*/
 				n = 0;
 				while ((pos = line.find(delimiter)) != std::string::npos) {
 					if (face_vectors.at(n).size() != getVectorSize())
@@ -61,22 +64,23 @@ void ORLData::loadDirectory(std::string path) {
 				}
 				line_number++;
 			}
-		}*/
+		}
 
 		Eigen::IOFormat fmt(2, Eigen::DontAlignCols, "\t", " ", "", "", "", "");
-		std::cout << face_vectors.at(0).format(fmt) << std::endl;
-		//visualiseImageVector(face_vectors.at(0));
+		std::cout << "First vector (size = " << face_vectors.at(0).size() << "): " << face_vectors.at(0).format(fmt) << std::endl;
+		int c = std::cin.get();
+		visualiseImageVector(face_vectors.at(0));
 
-		ocl_data_file.seekg(0);
+		/*ocl_data_file.seekg(0);
 		n = 0;
 		while (!ocl_data_file.eof()) {
 			face_vectors.at(n).resize(getVectorSize());
 			face_vectors.at(n).setZero();
 
-			/* Read vector by vector */
+			[> Read vector by vector <]
 			for (int i = 0; i < getVectorSize(); i++) {
 				value = "";
-				/* Read characters until tab encountered: one double value */
+				[> Read characters until tab encountered: one double value <]
 				do {
 					ocl_data_file.get(character);
 					value += character;
@@ -87,12 +91,12 @@ void ORLData::loadDirectory(std::string path) {
 
 			std::cout << face_vectors.at(0).format(fmt) << std::endl;
 			visualiseImageVector(face_vectors.at(0));
-			/* Go to next vector */
+			[> Go to next vector <]
 			n++;
 
-		}
+		}*/
 	} else {
-		std::cout << "/!\\ COULD NOT OPEN FILES /!\\";
+		std::cout << "/!\\ COULD NOT OPEN FILES /!\\" << std::endl;
 		exit(1);
 	}
 
