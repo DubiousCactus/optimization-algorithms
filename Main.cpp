91dca5e5d458ab5da3856f25a0c68f0e3f4a9ead
Theo Morales <theo.morales.fr@gmail.com>
2017-12-12 23:03:17 +0100

    Add PCA exports

--- Main.cpp
+++ Main.cpp
@@ -15,14 +15,15 @@ int main(int argc, char  **argv) {
 	std::vector<double> mnist_originalScores, mnist_originalExecTimes, mnist_pcaScores, mnist_pcaExecTimes;
 	std::vector<std::vector<double> > firstCSV, secondCSV;
 
-	/*std::cout << "--- Using ORL dataset: PCA version ---" << std::endl << std::endl;
+	std::cout << "--- Using ORL dataset: PCA version ---" << std::endl << std::endl;
 
 	ORLData *facesForPCA = new ORLData(40, 30, 40, 400);
-	facesForPCA->loadDirectory("/home/transpalette/Code/optimization-algorithms/ORL");
+	facesForPCA->loadDirectory("/home/transpalette/Code/Optimization_Project/ORL");
 	
 	Algorithm algoAPCA(facesForPCA);
 	algoAPCA.applyPCA();
-	orl_pcaExecTimes.push_back(algoAPCA.nearestClassCentroid());
+
+	/*orl_pcaExecTimes.push_back(algoAPCA.nearestClassCentroid());
 	orl_pcaScores.push_back(algoAPCA.calculateAccuracy() * 100);
 	orl_pcaExecTimes.push_back(algoAPCA.nearestSubClassCentroid(2));
 	orl_pcaScores.push_back(algoAPCA.calculateAccuracy() * 100);
@@ -66,15 +67,16 @@ int main(int argc, char  **argv) {
 	Algorithm::generateCSV("scores_and_times_ORL.csv", firstCSV);
 
 	int ch = std::cin.get();
-*/
+<]*/
 	std::cout << "--- Using MNIST dataset: PCA ---" << std::endl << std::endl;
 	
+	int ch = std::cin.get();
 	MNISTData *digitsPCA = new MNISTData(10, 28, 28);
-	digitsPCA->loadDirectory("/home/transpalette/Code/optimization-algorithms/MNIST"); //Use full path
+	digitsPCA->loadDirectory("/home/transpalette/Code/Optimization_Project/MNIST"); //Use full path
 
 	Algorithm algoBPCA(digitsPCA);
 	algoBPCA.applyPCA();
-	mnist_pcaExecTimes.push_back(algoBPCA.nearestClassCentroid());
+	/*mnist_pcaExecTimes.push_back(algoBPCA.nearestClassCentroid());
 	mnist_pcaScores.push_back(algoBPCA.calculateAccuracy() * 100);
 	mnist_pcaExecTimes.push_back(algoBPCA.nearestSubClassCentroid(2));
 	mnist_pcaScores.push_back(algoBPCA.calculateAccuracy() * 100);
@@ -88,8 +90,8 @@ int main(int argc, char  **argv) {
 	mnist_pcaScores.push_back(algoBPCA.calculateAccuracy() * 100);
 	mnist_pcaExecTimes.push_back(algoBPCA.perceptronMSE());
 	mnist_pcaScores.push_back(algoBPCA.calculateAccuracy() * 100);
-
-	std::cout << "--- Using MNIST dataset ---" << std::endl << std::endl;
+*/
+	/*std::cout << "--- Using MNIST dataset ---" << std::endl << std::endl;
 
 	MNISTData *digits = new MNISTData(10, 28, 28);
 	digits->loadDirectory("/home/transpalette/Code/optimization-algorithms/MNIST"); //Use full path
@@ -113,9 +115,8 @@ int main(int argc, char  **argv) {
 	secondCSV.push_back(mnist_originalScores);
 	secondCSV.push_back(mnist_originalExecTimes);
 	secondCSV.push_back(mnist_pcaScores);
-	secondCSV.push_back(mnist_pcaExecTimes);
+	secondCSV.push_back(mnist_pcaExecTimes);*/
 
-	Algorithm::generateCSV("scores_and_times_MNIST.csv", secondCSV);
 
 	return 0;
 }
